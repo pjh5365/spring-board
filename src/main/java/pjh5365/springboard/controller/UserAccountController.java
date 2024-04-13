68801a366465e3d48e13b91b6732dbe8034b3dc3
pjh5365 <pjh5365@naver.com>
2024-04-13 17:25:32 +0900

    Feat: 회원가입, 로그인 기능 추가
    Related to: #11

---
+++ src/main/java/pjh5365/springboard/controller/UserAccountController.java
@@ -0,0 +1,68 @@
+package pjh5365.springboard.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import pjh5365.springboard.dto.UserAccountRequest;
+import pjh5365.springboard.entity.UserAccount;
+import pjh5365.springboard.service.UserAccountService;
+
+/**
+ * @author : parkjihyeok
+ * @since : 2024/04/13
+ */
+@Slf4j
+@Controller
+public class UserAccountController {
+
+	private final UserAccountService userAccountService;
+
+	@Autowired
+	public UserAccountController(UserAccountService userAccountService) {
+		this.userAccountService = userAccountService;
+	}
+
+	@GetMapping("/join")
+	public String joinPage() {
+		return "joinPage";
+	}
+
+	@PostMapping("/join")
+	public String joinAction(UserAccountRequest userAccountRequest) {
+		userAccountService.join(userAccountRequest);
+		log.info("회원가입 {}", userAccountRequest);
+		return "redirect:/login";
+	}
+
+	@GetMapping("/login")
+	public String loginPage() {
+		return "loginPage";
+	}
+
+	@PostMapping("/login")
+	public String loginAction(HttpServletRequest request, UserAccountRequest userAccountRequest) {
+		UserAccount user = userAccountService.login(userAccountRequest);
+		if (user == null) {
+			return "redirect:/login";
+		}
+		HttpSession session = request.getSession();
+		session.setAttribute("userId", user.getUserId());
+		session.setMaxInactiveInterval(300);
+		log.info("로그인 {}", userAccountRequest);
+		return "redirect:/";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			session.invalidate();
+		}
+		return "redirect:/";
+	}
+}
---
+++ src/main/java/pjh5365/springboard/service/UserAccountService.java
@@ -0,0 +1,42 @@
+package pjh5365.springboard.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pjh5365.springboard.dto.UserAccountRequest;
+import pjh5365.springboard.entity.UserAccount;
+import pjh5365.springboard.repository.UserAccountRepository;
+
+/**
+ * @author : parkjihyeok
+ * @since : 2024/04/13
+ */
+@Service
+public class UserAccountService {
+
+	private final UserAccountRepository userAccountRepository;
+
+	@Autowired
+	public UserAccountService(UserAccountRepository userAccountRepository) {
+		this.userAccountRepository = userAccountRepository;
+	}
+
+	public void join(UserAccountRequest request) {
+		UserAccount user = UserAccount.builder().userId(request.getUserId()).password(request.getPassword()).build();
+		userAccountRepository.save(user);
+	}
+
+	public UserAccount login(UserAccountRequest request) {
+		String userId = request.getUserId();
+		Optional<UserAccount> getUser = userAccountRepository.findByUserId(userId);
+		if (getUser.isPresent()) {
+			UserAccount user = getUser.get();
+			if (user.getPassword().equals(request.getPassword())) {
+				return user;
+			}
+		}
+		return null;
+	}
+}
